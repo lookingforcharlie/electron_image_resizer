46443a5ac499a9c52c9500d02b802852045f749b
lookingforcharlie <charliefeng2020@gmail.com>
2024-04-05 13:34:39 -0400

    Setup the project

---
+++ README.md
@@ -0,0 +1,16 @@
+# Electron Desktop App
+
+- Electron, JavaScript
+
+## Development
+
+- Watch and restart/reload electron applications
+  ```
+  npx electronmon .
+  ```
+
+## Learning Electron
+
+- With Electron, we have something called main process and render process.
+  - Main process is like a backend.
+  - Render process is like the frontend, such as frontend UI and JavaScript.
---
+++ main.js
@@ -0,0 +1,66 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+const isDev = process.env.NODE_ENV !== 'production';
+// This is true if you are on Mac
+const isMac = process.platform === 'darwin';
+
+// Create the main window
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    title: 'Image Resizer',
+    width: isDev ? 1000 : 500,
+    Height: 600,
+  });
+
+  // Open devtools if in dev env
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  // We don't need path to help here
+  // mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
+  mainWindow.loadFile('./renderer/index.html');
+
+  // We can load a web page in the app
+  // mainWindow.loadURL('https://meetxb.com');
+}
+
+// app is ready
+app.whenReady().then(() => {
+  createMainWindow();
+
+  // Implement menu
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
+  // When the app is activated, if there's no window, make sure we create the main window
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+    }
+  });
+});
+
+// menu template
+const menu = [
+  {
+    role: 'fileMenu',
+  },
+];
+
+// Another way to setup the menu template
+// const menu = [
+//   {
+//     label: 'File',
+//     submenu: [
+//       { label: 'Quit', click: () => app.quit(), accelerator: 'CmdOrCtrl+w' },
+//     ],
+//   },
+// ];
+
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
